93807c66b311a39afac119b50e6c73143216ce88
anishywa <anishfundu@gmail.com>
2025-09-02 21:31:57 -0400

    Initial Commit

---
+++ HarvardArtAPP/HarvardArtAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarvardArtAPPApp.swift
+//  HarvardArtAPP
+//
+//  Created by Anish Sharma on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct HarvardArtAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HarvardArtAPPTests/HarvardArtAPPTests.swift
@@ -0,0 +1,16 @@
+//
+//  HarvardArtAPPTests.swift
+//  HarvardArtAPPTests
+//
+//  Created by Anish Sharma on 9/2/25.
+//
+
+import Testing
+
+struct HarvardArtAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
